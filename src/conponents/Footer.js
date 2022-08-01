3081f7bbb3f85b206e8f6acb49d0e6f1ca7c804c
alazartegene <alazartegene@gmail.com>
2022-08-01 21:26:50 +0300

    final commit

---
+++ src/conponents/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Styles from "../App.module.css";
+const Footer = () => {
+  return (
+    <div className={Styles.footer}>
+      <p>
+        <span>Frank</span>
+      </p>
+      <p>api.github.com/user</p>
+    </div>
+  );
+};
+
+export default Footer;
